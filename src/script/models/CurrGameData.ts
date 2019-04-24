9eb9df98470d608aad447abd43c357949321423b
hekang <490387055@qq.com>
2019-04-24 10:59:23 +0800

    no message

--- src/script/models/CurrGameData.ts
+++ src/script/models/CurrGameData.ts
@@ -1,8 +1,10 @@
+import { SORTTYPE } from "./ConfigData";
+
 //当前游戏的数据
 export default class CurrGameData{
     
     public score:number = 0;//分数
- 
+    public sortType:SORTTYPE = SORTTYPE.ENDLESS; //游戏类型
     public reset() {
         this.score = 0;
     }
